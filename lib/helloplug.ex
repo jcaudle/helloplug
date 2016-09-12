16bdf6051d64411d0d5ef7b169a5b893151f93be
Joseph Caudle <joseph@josephcaudle.com>
2016-09-11 21:31:38 -0400

    Hello, world!

--- lib/helloplug.ex
+++ lib/helloplug.ex
@@ -1,2 +1,11 @@
 defmodule Helloplug do
+  def init(default_opts) do
+    IO.puts "starting up Helloplug..."
+    default_opts
+  end
+
+  def call(conn, _opts) do
+    IO.puts "saying hello!"
+    Plug.Conn.send_resp(conn, 200, "Hello, world!")
+  end
 end
